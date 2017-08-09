f72dc01d380321cfcbf3c4fd4007855af5264693
zp741 <canppei@hotmail.com>
2017-08-09 15:55:04 +0800

    测试

--- models/Delivery.js
+++ models/Delivery.js
@@ -12,7 +12,8 @@ var Delivery = new keystone.List('Delivery', {
 
 Delivery.add({
   "自动编号": {type: Number, label: '自动编号'},
-    name: { type: String, required: true, label:'发送材料' },
+    name: { type: String, label:'发送材料' },
+    必填字段: { type: String, required: true, label:'必填字段' },
 });
 
 Delivery.relationship({ ref: 'Case', path: 'Delivery' });
