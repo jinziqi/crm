1c1106d1bd954b12b064d5302ece2080be344940
zp741 <canppei@hotmail.com>
2017-08-11 09:50:33 +0800

    添加证据保全表字段

--- models/EvidenceProve.js
+++ models/EvidenceProve.js
@@ -14,25 +14,11 @@ var EvidenceProve = new keystone.List('EvidenceProve', {
 
 EvidenceProve.add({
         name: {type: String, required: true, label: '证书编号'},
+        "主要保全内容": {type: String, label: '主要保全内容'},
+        "保全成本": {type: Types.Money, label: '保全成本'},
+        "保全附加费": {type: Types.Money, label: '保全附加费'},
+        "结算申请日期": {type: Types.Date, label: '结算申请日期'},
     }
 );
-
-EvidenceProve.customFilter = function (where, user) {
-    if (!user.isAdmin)
-        where.accessUsers = {$in: [user._id]};
-    return where;
-};
-
-EvidenceProve.schema.pre('save', function (next) {
-    if (this.isNew) {
-        if (this.accessUsers.length === 0 && this._req_user) {
-            this.accessUsers.push(this._req_user._id);
-        }
-    }
-    next();
-});
-
-EvidenceProve.permission = true;
-
 EvidenceProve.defaultColumns = 'name';
 EvidenceProve.register();
