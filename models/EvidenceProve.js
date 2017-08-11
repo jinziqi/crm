4c052513781e8f8670e11df7c62d10531dabc6e5
zp741 <canppei@hotmail.com>
2017-08-11 09:39:50 +0800

    继续修改证据保全表

--- models/EvidenceProve.js
+++ models/EvidenceProve.js
@@ -2,7 +2,7 @@
 var Types = keystone.Field.Types;
 
 /**
- * Case Model
+ * EvidenceProve Model
  * ==========
  */
 
@@ -13,17 +13,17 @@ var EvidenceProve = new keystone.List('EvidenceProve', {
 });
 
 EvidenceProve.add({
-        name: {type: String, required: true, label: '证书编号'},        
+        name: {type: String, required: true, label: '证书编号'},
     }
 );
 
-Case.customFilter = function (where, user) {
+EvidenceProve.customFilter = function (where, user) {
     if (!user.isAdmin)
         where.accessUsers = {$in: [user._id]};
     return where;
 };
 
-Case.schema.pre('save', function (next) {
+EvidenceProve.schema.pre('save', function (next) {
     if (this.isNew) {
         if (this.accessUsers.length === 0 && this._req_user) {
             this.accessUsers.push(this._req_user._id);
@@ -32,7 +32,7 @@ Case.schema.pre('save', function (next) {
     next();
 });
 
-Case.permission = true;
+EvidenceProve.permission = true;
 
-Case.defaultColumns = 'name|200px,案件编号|75px,案件批次,侵权形式,图片编号,侵权页面链接,发现人,线索处理意见,线索放弃原因,负责律所,诉讼状态,判决/调解赔偿金额,放弃诉讼原因,案件特别提醒';
-Case.register();
+EvidenceProve.defaultColumns = 'name';
+EvidenceProve.register();
