a5a8dfc357d1671e2331491e617746ddf4867583
zp741 <canppei@hotmail.com>
2017-08-14 14:21:59 +0800

    调整

--- models/Delivery.js
+++ models/Delivery.js
@@ -15,6 +15,7 @@ Delivery.add({
     "材料类型": {type: Types.Select, options: '数码原图,胶片,委托创作说明,肖像权协议,资质证件,版权登记证书,票据,法律服务协议,其他', label: '材料类型'},
     "对应被告": { type: String, label: '对应被告' },
     "对应图片编号": { type: String, label: '对应图片编号' },
+    "对应案件编号": { type: Number, label: '对应案件编号' },
 });
 
 Delivery.defaultColumns = 'name,材料类型,发送或收取,材料载体数量,收件单位,收件日期';
